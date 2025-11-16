d42d18dd4119a4e2195ad33852b2bd50b4ec0d5b
coptrzz <nwaclos@gmail.com>
2025-11-16 20:05:03 +0300

    done

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -170,22 +170,45 @@
 import mongoose from "mongoose";
 
 // establish connection
-
+const uri =
+  "mongodb+srv://nwaclos_db1test:Asdf%4022@cluster0.bmq7loy.mongodb.net/labDB";
+mongoose.connect(uri);
 
 // define schema
-
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  major: String
+});
+const Student = mongoose.model("Student", studentSchema);
 
 // create document
-
+async function createStudents() {
+  await Student.insertMany([
+    { name: "Ali", age: 21, major: "CS" },
+    { name: "Sara", age: 23, major: "SE" }
+  ]);
+  console.log("✅ Inserted");
+}
+createStudents();
 
 // read document
-
+async function readStudents() {
+  const all = await Student.find();
+  console.log(all);
+}
+readStudents();
 
 // update document
-
+async function updateStudent() {
+  await Student.updateOne({ name: "Ali" }, { age: 22 });
+  console.log("✅ Updated Ali");
+}
+updateStudent();
 
 // delete document
-
-
-
-
+async function deleteStudent() {
+  await Student.deleteOne({ name: "Sara" });
+  console.log("✅ Deleted Sara");
+}
+deleteStudent();
